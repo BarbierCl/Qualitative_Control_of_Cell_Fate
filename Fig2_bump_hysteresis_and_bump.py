18ad7b2f1d48a3367f5b2bad005e64b389331fab
BarbierCl <claire.barbier@unice.fr>
2025-07-22 12:40:56 +0200

    Add files via upload

---
+++ Fig2_bump_hysteresis_and_bump.py
@@ -0,0 +1,97 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sympy import symbols, Eq, solve
+
+"""
+Goal : to plot the histeresis of the 1D model and the bump signal used
+"""
+#driving signal
+pb=3.5
+p0=4
+pmax=5
+pe=4.5
+xup0=1.44
+xs1p0=0.62
+xs2p0=3.94
+dpi=300 #graphic resolution
+# y values for the histeresis plot
+y = np.linspace(0, 4.2, 300)
+ftsize=11 #size of caracters
+p=(y**3-6*y**2+9*y+0.5)
+# Plotting
+plt.plot(p, y,linewidth=1.5)
+plt.plot([4.5,4.5], [0,1],linestyle='--',color='red')
+plt.text(4.3,-1.2,r'$p^*=4.5$',color='red',fontsize=ftsize,rotation=-60)
+plt.plot([6,6], [0,4.15],linestyle='--',color='red')
+plt.text(5.6,-1.2,r'$p_{max}=6$',color='red',fontsize=ftsize,rotation=-60)
+plt.plot([0.5,0.5], [0,3],linestyle='--',color='red')
+plt.text(0.4,-1.2,r'$p^{**}=0.5$',color='red',fontsize=ftsize,rotation=-60)
+plt.plot([p0,p0], [0,xs2p0],linestyle='--',color='red')
+plt.text(3.5,-1,fr'$p_0={p0}$',color='red',fontsize=ftsize,rotation=-60)
+plt.plot([0,p0], [xup0,xup0],linestyle='--',color='red')
+plt.text(-1.5,1.4,fr'${{x}}^{{u}}_{{p_0}}\simeq {xup0}$',color='red',fontsize=ftsize)
+plt.plot([0,p0], [xs2p0,xs2p0],linestyle='--',color='red')
+plt.text(-1.5,3.8,fr'${{x}}^{{s_2}}_{{p_0}}\simeq {xs2p0}$',color='red',fontsize=ftsize)
+plt.plot([0,p0], [xs1p0,xs1p0],linestyle='--',color='red')
+plt.text(-1.5,0.6,fr'${{x}}^{{s_1}}_{{p_0}}\simeq {xs1p0}$',color='red',fontsize=ftsize)
+plt.plot([0,0.5], [3,3],linestyle='--',color='purple')
+plt.text(-1.3,3,r'$x^{**}=3$',color='purple',fontsize=ftsize)
+plt.plot([0,4.5], [1,1],linestyle='--',color='purple')
+plt.text(-1.3,1,r'$x^{*}=1$',color='purple',fontsize=ftsize)
+plt.xlabel('$p$ (a.u.)',loc='right')
+plt.ylabel('$y$ (a.u.)',loc='top')
+plt.yticks([0, 2,5]) 
+plt.xticks([0,1,3,7])
+plt.tight_layout()
+plt.savefig('Fig2_NI_memory_system.png', dpi=dpi)
+plt.show()
+
+
+
+#driving signal
+m=2
+tmin=0
+tmax=5
+sigma=4
+delta=2*np.sqrt(-sigma**2*np.log(1+(p0-pmax)/(pmax-pb)*(1-np.exp(-m**2/sigma**2))))
+t=np.linspace(tmin,tmax,1000)
+
+p=(pmax-pb)/(1-np.exp(-m**2/sigma**2))*(np.exp(-(t-m)**2/sigma**2)-1)+pmax
+
+indp0= np.where(np.diff(np.sign( p- p0)))[0]
+indpe= np.where(np.diff(np.sign( p- pe)))[0]
+indpb= np.where(np.diff(np.sign( p- pb)))[0]
+dd=t[indp0[1]]-t[indp0[0]]
+
+
+plt.figure(2)
+plt.plot(t,p)
+plt.plot([0,t[indp0[1]]], [p0,p0],linestyle='--',color='red')
+plt.text(-1.1, p0, fr'$p_0 = {p0}$', color='red', fontsize=ftsize)
+plt.plot([0,t[indpb[1]]], [pb,pb],linestyle='--',color='red')
+plt.text(-1.1,pb,fr'$p_b= {pb}$',color='red',fontsize=ftsize)
+plt.plot([0,t[indpe[1]]], [pe,pe],linestyle='--',color='red')
+plt.text(-1.2,pe,fr'$p^*={pe}$',color='red',fontsize=ftsize)
+plt.plot([0,m], [pmax,pmax],linestyle='--',color='red')
+plt.text(-1.2,pmax,fr'$p_{{max}}= {pmax}$',color='red',fontsize=ftsize)
+plt.plot([t[indp0[0]],t[indp0[0]]], [2,p0],linestyle='--',color='red')
+plt.text(t[indp0[0]]-0.1,1.1,fr'$t_0={round(t[indp0[0]],2)}$',color='red',fontsize=ftsize,rotation=-60)
+plt.plot([t[indp0[1]],t[indp0[1]]], [2,p0],linestyle='--',color='red')
+plt.text(t[indp0[1]]-0.1,1,fr'$t_1={round(t[indp0[1]],2)}$',color='red',fontsize=ftsize,rotation=-60)
+plt.plot([t[indpe[1]],t[indpe[1]]], [2,pe],linestyle='--',color='red')
+plt.text(t[indpe[1]]-0.1,1.1,fr'$t_2^*={round(t[indpe[1]],2)}$',color='red',fontsize=ftsize,rotation=-60)
+plt.plot([t[indpe[0]],t[indpe[0]]], [2,pe],linestyle='--',color='red')
+plt.text(t[indpe[0]]-0.1,1,fr'$t_1^*={round(t[indpe[0]],2)}$',color='red',fontsize=ftsize,rotation=-60)
+plt.plot([m,m], [2,pmax],linestyle='--',color='red')
+plt.text(m,1.1,r'$t_{max}=2$',color='red',fontsize=ftsize,rotation=-60)
+plt.plot([0,0], [2,2],linestyle='--',color='red')
+plt.text(-0.3,1.3,r'$t_{i}=0$',color='red',fontsize=ftsize,rotation=-60)
+#plt.plot([tmax,tmax], [2,pmax],linestyle='--',color='red')
+plt.text(tmax-0.1,1.1,r'$t_{f}=5$',color='red',fontsize=ftsize,rotation=-60)
+plt.xticks([6])             # Positions des ticks sur l'axe x
+plt.yticks([3,6]) 
+plt.xlabel('t (a.u.)',loc='right')
+plt.ylabel(' $p$ (a.u.)',loc='top')
+plt.tight_layout()
+plt.savefig('Fig2_NI_dynamic_signal.png', dpi=dpi)
+plt.show()
---
+++ Fig3_bump_size_receptive_set.py
@@ -0,0 +1,120 @@
+
+
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+from scipy.integrate import odeint
+from math import exp
+
+
+#%%
+vect_invR=[]
+nT=500 #nb of different delta
+#vect_delta=np.linspace(0.1,2,nT) #vector of durations
+vect_sigma=np.linspace(0.001,5,nT)
+xup0=1.44#1.83
+nxi=1000 #nb of inital values
+tmin=0 #minimal time
+#parameters for the driving signal
+xs1p0=0.62#0.36 #calculated on the 1D model
+#parameters for the driving signal
+pb=3.5 #basal value
+p0=4 #value for the first return map
+pmax=5 #max value
+m=2 #center of the signal
+
+vect_y0=np.linspace(0,xup0,nxi)
+def F(y,t,sigma):
+    #args:
+    #y=initial condition
+    #t=time vector for the resolution
+    #delta=duration of the driving signal
+    #return : solution of the differential system
+    p=(pmax-pb)/(1-np.exp(-m**2/sigma**2))*(np.exp(-(t-m)**2/sigma**2)-1)+pmax
+    dydt=p-(y**3-6*y**2+9*y+0.5)
+    return dydt
+
+vect_delta=[]
+
+def g_dicho(y0,sigma):
+    #args : 
+    #y0=initial condition 
+    #delta=duration of the signal
+    #return : (xup0-R(x))
+    delta=2*np.sqrt(-sigma**2*np.log(1+(p0-pmax)/(pmax-pb)*(1-np.exp(-m**2/sigma**2))))
+    t=np.linspace(0,m+delta/2,1000)
+    y=odeint(F,y0,t,(sigma,))
+    RT=y[-1,0]
+    return((xup0-RT))
+def dicho(sigma,eps):
+    #Bisection method to find the zero of g_dicho
+    #args:
+    #delta=duration of the driving signal
+    #eps=tolerance for the algorithm
+    ya=0
+    yb=xup0
+    
+    while(abs(yb-ya)>eps):
+        ym=(ya+yb)/2
+        if(g_dicho(ym,sigma))>=0:
+            ya=ym
+        else:
+            yb=ym
+    return(ym)
+eps=0.0001
+vect_delta=[]
+vect_invR=[]
+for i in range(len(vect_sigma)):
+    sigma=vect_sigma[i]
+    delta=2*np.sqrt(-sigma**2*np.log(1+(p0-pmax)/(pmax-pb)*(1-np.exp(-m**2/sigma**2))))
+    vect_delta.append(delta)
+    xr=dicho(sigma,eps)
+    vect_invR.append(xr)
+
+
+#find Tr and Tx
+#ind_Tx= np.where(np.diff(np.sign( np.array(vect_invR)-xs1p0)))[0] 
+ind_Tx= np.where(np.diff(np.sign( np.array(vect_invR)-1.25)))[0]
+ind_Tr= np.argmin(np.abs(vect_invR))
+val_max = np.max(vect_invR)
+ind_Ti= np.where(vect_invR == val_max)[0]
+Tx=round(vect_delta[ind_Tx[0]],2)
+Tr=round(vect_delta[ind_Tr],2)
+Ti=round(vect_delta[ind_Ti[-1]],2)
+dpi=300 #dpi resoultion for graphics
+plt.figure(1)
+fig, ax = plt.subplots()
+ax.plot(vect_delta,vect_invR,color='red')
+ax.plot([min(vect_delta),max(vect_delta)], [xup0,xup0],linestyle='--',color='red')
+mask = (np.array(vect_delta)<= 2.44) & (np.array(vect_delta) >= 0)#color blue zone
+arr_vect_invR=np.array(vect_invR)
+vect_xup0=np.linspace(xup0,xup0,len(vect_invR))
+mask2= (np.array(vect_delta) >= min(vect_delta)) & (np.array(vect_delta) <= max(vect_delta))#color green zone
+plt.fill_between(np.array(vect_delta)[mask2], arr_vect_invR[mask2], vect_xup0[mask2], color='lightgreen', alpha=0.5)
+plt.fill_between(np.array(vect_delta)[mask], arr_vect_invR[mask], color='skyblue', alpha=0.5)
+plt.text(min(vect_delta)-0.8,xup0,r'$x_{p_0}^u \simeq 1.44$',color='red',fontsize=11)
+plt.text(2.7,0.75,r'$I_{recep}$',color='green')
+plt.text(1,0.75,r'$I_{insens}$',color='blue')
+plt.text(min(vect_delta)-0.7,1.25,r'$\bar{x}=1.25$')
+#plt.text(min(vect_delta)-0.7,xs1p0,r'$x^{s_1}_{p_0} \simeq 0.62$')
+#plt.plot([Ti,Ti],[0,xup0],linestyle='--',color='red')
+#plt.plot([min(vect_delta),Ti],[xup0,xup0],color='red',linestyle='--')
+plt.plot([2.37,2.37],[0,1.25],linestyle='--',color='black')
+plt.plot([min(vect_delta),2.37],[1.25,1.25],color='black',linestyle='--')
+#plt.plot([Tx,Tx],[0,xs1p0],linestyle='--',color='black')
+#plt.plot([min(vect_delta),Tx],[xs1p0,xs1p0],color='black',linestyle='--')
+plt.xlabel(' $\delta_{p_0} (a.u.)$',loc='right')
+plt.ylabel('$R^{-1}(x_{p_0}^u)$\n (a.u.)',rotation=90,loc='top')
+ax.text(2.4, ax.get_ylim()[0] - 0.01, r'$\tau_L(\bar{x})$', ha='center', va='top',fontsize=11,rotation=-45)
+ax.text(2.6, ax.get_ylim()[0]-0.02 , '$T^r$', ha='center', va='top',fontsize=11,color='red')
+plt.yticks([0, 0.5,1,2]) 
+plt.xticks([0,1,2,3],rotation=-45)
+plt.text(2, 1.9, fr"$\tau_{{L}}(\bar{{x}})\simeq {Tx}$")
+#plt.text(2,1.9,fr"$\tau_{L}(\bar{{x}})\simeq {Tx}$")
+#plt.text(Ti-0.1,-0.17,fr"$T^{{i}}$",color='red')
+plt.text(2,1.7,fr"$T^r\simeq{Tr}$",color='black')
+#plt.text(2,1.5,fr"$T^i\simeq{Ti}$",color='black')
+plt.tight_layout()
+plt.savefig('Fig3_NI_length_receptive_set.png', dpi=dpi)
+plt.show
